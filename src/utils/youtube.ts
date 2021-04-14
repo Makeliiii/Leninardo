bd67ee9f4e65e9b2f39bf09d4a5feb5eda569107
Mäkeli <joel.m.maenpaa@gmail.com>
2021-04-14 15:14:09 +0300

    youtube search function

---
+++ src/utils/youtube.ts
@@ -0,0 +1,14 @@
+import { Video, YouTube } from "popyt";
+import isUrl from "is-url";
+
+const yt = new YouTube(process.env.YOUTUBE);
+
+export const search = async (videoUrl: string): Promise<Video | undefined> => {
+  try {
+    if (isUrl(videoUrl)) {
+      return yt.getVideo(videoUrl);
+    }
+  } catch (error) {
+    return error;
+  }
+};
