f118dd9ce5904678211a0245756f98a682030dff
Mäkeli <joel.m.maenpaa@gmail.com>
2021-04-14 14:43:27 +0300

    github command and remove pokemon commands cause they're useless

---
+++ src/commands/utils/GitHub.ts
@@ -0,0 +1,17 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+
+export default class GitHub extends Command {
+  constructor() {
+    super("github", {
+      aliases: ["github", "gh", "repo", "repository"],
+      category: "util",
+      description: "GitHub link.",
+    });
+  }
+
+  async exec(msg: Message) {
+    const repo = "https://github.com/Makeliiii/Leninardo";
+    return msg.channel.send(repo);
+  }
+}
