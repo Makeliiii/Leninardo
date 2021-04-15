3c947810c732cd1af80a6d273971eb9b84b97ff4
Mäkeli <joel.m.maenpaa@gmail.com>
2021-04-15 16:31:43 +0300

    resume command

---
+++ src/commands/music/Resume.ts
@@ -0,0 +1,25 @@
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+
+export default class Resume extends Command {
+  constructor() {
+    super('resume', {
+      aliases: ['resume', 'jatka'],
+      ratelimit: 1,
+      category: 'music',
+      description: 'Resume the current song.',
+    });
+  }
+
+  async exec(msg: Message) {
+    const channel = msg.member?.voice.channel;
+    const serverQue = this.client.queue;
+
+    if (!channel)
+      return msg.channel.send('You must be in a voice channel to pause music!');
+    if (!serverQue.length) return msg.channel.send(`There's nothing playing!`);
+
+    this.client.voiceConnection?.dispatcher.resume();
+    return msg.channel.send(`**${serverQue[0].title}** resumed!`);
+  }
+}
