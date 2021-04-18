779c01a134bf9af43046bfa59963d2ab193ac8a3
Mäkeli <joel.m.maenpaa@gmail.com>
2021-04-18 16:46:34 +0300

    deprecation warnings

--- src/utils/mongoConnection.ts
+++ src/utils/mongoConnection.ts
@@ -4,5 +4,6 @@ export const connect = async (mongoUri: string) => {
   return await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   });
 };
