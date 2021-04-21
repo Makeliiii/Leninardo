bd4cfdc087a29adf2cb9bef0a5a1e0843d26a6e3
Mäkeli <joel.m.maenpaa@gmail.com>
2021-04-21 15:18:14 +0300

    add function to modify command

--- src/commands/music/ModifyPlaylist.ts
+++ src/commands/music/ModifyPlaylist.ts
@@ -1,10 +1,6 @@
 import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
-import { PlaylistDocument } from '../../interfaces/PlaylistDocument';
-import { SongDocument } from '../../interfaces/SongDocument';
-import { Playlist } from '../../models/Playlist';
-import { findBySpecificTitle } from '../../utils/mongoOperations';
-import { searchByUrl } from '../../utils/youtube';
+import { findByTitleAndUpdate } from '../../utils/mongoOperations';
 
 interface Args {
   title: string;
@@ -53,34 +49,12 @@ export default class ModifyPlaylist extends Command {
   async exec(msg: Message, { title, add, remove, sort, content }: Args) {
     if (!title) return msg.channel.send('Provide a title to search by!');
 
-    const playlist = await findBySpecificTitle(title);
-
-    if (!playlist)
-      return msg.channel.send(
-        `Could not find a playlist with the title: ${title}`,
-      );
-
     if (add) {
       const songs = content.split(' ');
-      // any cause i can't really use the "SongDocument" interface here
-      // as it will be missing a bunch of methods and such cause it
-      // extends the "Document" interface from mongooses typings zzz
-      for (const song of songs) {
-        await searchByUrl(song).then(async (song) => {
-          // read comment above, same applies
-          const newSong = <SongDocument>{
-            title: song.title,
-            url: song.url,
-          };
-
-          playlist.songs.push(newSong);
-        });
-      }
-
-      await playlist.save();
-      return msg.channel.send(`Saved songs to playlist: ${playlist.title}.`);
+      return await findByTitleAndUpdate(title, songs).then((playlist) => {
+        if (typeof playlist === 'string') return msg.channel.send(playlist);
+        return msg.channel.send(`Added songs to playlist ${playlist.title}`);
+      });
     }
-
-    return msg.channel.send(`Found ${playlist.title}.`);
   }
 }
