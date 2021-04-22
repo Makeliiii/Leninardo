f26a1ff6500eee4a8588702656ed0a4bfba34d5c
Mäkeli <joel.m.maenpaa@gmail.com>
2021-04-22 12:55:11 +0300

    list playlist songs

---
+++ src/commands/music/ListPlaylistSongs.ts
@@ -0,0 +1,33 @@
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+import { findBySpecificTitle } from '../../utils/mongoOperations';
+
+export default class ListPlaylistSongs extends Command {
+  public constructor() {
+    super('listplaylistsongs', {
+      aliases: ['lps', 'listsongs', 'listplaylistsongs', 'listps'],
+      ratelimit: 1,
+      category: 'music',
+      description: 'List songs in a specific playlist.',
+      args: [
+        {
+          id: 'title',
+          type: 'string',
+        },
+      ],
+    });
+  }
+
+  async exec(msg: Message, { title }: { title: string }) {
+    if (!title) return msg.channel.send('Title param required.');
+    const playlist = await findBySpecificTitle(title);
+    if (!playlist) return msg.channel.send('Playlist not found!');
+    const songs = playlist.songs.map((song, index) => {
+      return `${index + 1}. ${song.title}\n`;
+    });
+
+    return msg.channel.send(
+      `**${playlist.title}** songs:\n\n${songs.join('')}`,
+    );
+  }
+}
